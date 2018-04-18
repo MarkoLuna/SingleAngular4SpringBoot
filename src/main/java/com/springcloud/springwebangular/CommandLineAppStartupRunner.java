18e7ac467cfbc50b2a8ff1287842833bcd686e88
Marcos Luna <e.al.t@hotmail.com>
2018-04-18 10:30:51 -0500

    update project

--- src/main/java/com/springcloud/springwebangular/CommandLineAppStartupRunner.java
+++ src/main/java/com/springcloud/springwebangular/CommandLineAppStartupRunner.java
@@ -1,30 +1,33 @@
 package com.springcloud.springwebangular;
 
-import com.springcloud.springwebangular.pojos.Person;
-import com.springcloud.springwebangular.respository.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.springcloud.springwebangular.pojos.Person;
+import com.springcloud.springwebangular.respository.PersonRepository;
+
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
 
-    private static final Logger logger = LoggerFactory.getLogger(CommandLineAppStartupRunner.class);
+	private static final Logger logger = LoggerFactory.getLogger(CommandLineAppStartupRunner.class);
 
-    @Autowired
-    PersonRepository personRepository;
+	@Autowired
+	PersonRepository personRepository;
 
-    public CommandLineAppStartupRunner(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
+	public CommandLineAppStartupRunner(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        personRepository.save(new Person("Marcos", "Luna"));
-        personRepository.save(new Person("Gerardo", "Luna"));
-        personRepository.save(new Person("YO2", "Valdez"));
+	@Override
+	public void run(String... args) throws Exception {
+		personRepository.save(new Person("Marcos", "Luna"));
+		personRepository.save(new Person("Gerardo", "Luna"));
+		personRepository.save(new Person("YO2", "Valdez"));
 
-    }
+		personRepository.findAll().forEach(person -> logger.info(person.toString()));
+
+	}
 }
--- src/main/java/com/springcloud/springwebangular/pojos/Person.java
+++ src/main/java/com/springcloud/springwebangular/pojos/Person.java
@@ -8,33 +8,40 @@ import javax.persistence.Id;
 @Entity
 public class Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    private String firstName;
-    private String lastName;
+	private String firstName;
+	private String lastName;
 
-    public Person() {}
+	public Person() {
+	}
 
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
 }
